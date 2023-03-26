6bb35eb69857cffa344d336576b7707e57ba7774
vsomova <valgsom@gmail.com>
2023-03-26 15:32:01 -0400

    Get reports for undersampling

--- code/24-feat-sel.py
+++ code/24-feat-sel.py
@@ -3,8 +3,6 @@ from sklearn.feature_selection import SelectKBest, chi2
 import importlib
 apply_ML = importlib.import_module("22-apply-ML") # import the file so we can use functions from it
 
-#  INCLUDE UNDERSAMPLING, WRITE A REPORT FOR THAT
-
 def FS(df, N_features):
 
     df = df.drop(columns=["~~n_posts~~", "~~n_words~~"])  # remove these columns
@@ -21,41 +19,56 @@ def FS(df, N_features):
     return new_df
 
 
-def write_report(N_features):
+def write_report(N_features, resample):
 
     report = ""
 
     df1 = pd.read_csv(f"../results/lemmas_normalized/norm_by_freq.csv", keep_default_na=False, index_col=0)
     new_df1 = FS(df1, N_features) # apply feature selection
-    report += apply_ML.eval_norm_freq(new_df1) # get a report for ML for new df
+    report += apply_ML.eval_norm_freq(new_df1, resample) # get a report for ML for new df
 
     report += "\n\n"
 
     df2 = pd.read_csv(f"../results/lemmas_normalized/norm_by_pres.csv", keep_default_na=False, index_col=0)
     new_df2 = FS(df2, N_features) # apply feature selection
-    report += apply_ML.eval_norm_pres(new_df2)  # get a report for ML for new df
+    report += apply_ML.eval_norm_pres(new_df2, resample)  # get a report for ML for new df
 
     report += "\n\n"
 
     df3 = pd.read_csv(f"../results/lemmas_normalized/norm_by_med.csv", keep_default_na=False, index_col=0)
     new_df3 = FS(df3, N_features) # apply feature selection
-    report += apply_ML.eval_norm_med(new_df3)  # get a report for ML for new df
+    report += apply_ML.eval_norm_med(new_df3, resample)  # get a report for ML for new df
 
-    with open(f"../results/ML_reports/report_{N_features}.txt", "w") as f:
-        f.write(report)
+    if resample == "undersample":
+        with open(f"../results/ML_reports/report_{N_features}_undersampled.txt", "w") as f:
+            f.write(report)
+    else:
+        with open(f"../results/ML_reports/report_{N_features}.txt", "w") as f:
+            f.write(report)
 
 
 def main():
     # originally we have 6929 features, now we try to reduce them
 
     N_features = 3464  # cut number of desired features in half
-    write_report(N_features)
+    write_report(N_features, None)
 
     N_features = 1000  # try lower number of desired features
-    write_report(N_features)
+    write_report(N_features, None)
 
     N_features = 100  # try lower number of desired features
-    write_report(N_features)
+    write_report(N_features, None)
 
+
+#     try with resampling
+
+    N_features = 3464  # cut number of desired features in half
+    write_report(N_features, "undersample")
+
+    N_features = 1000  # try lower number of desired features
+    write_report(N_features, "undersample")
+
+    N_features = 100  # try lower number of desired features
+    write_report(N_features, "undersample")
 
 main()
