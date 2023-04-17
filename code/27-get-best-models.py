e3ac7751ee97c991ac99c2efff301487a85eb0f1
vsomova <valgsom@gmail.com>
2023-04-17 17:51:33 -0400

    Choose the best performing models

---
+++ code/27-get-best-models.py
@@ -0,0 +1,12 @@
+# we want to minimize false negatives so we choose models with high precision
+
+import pandas as pd
+
+df = pd.read_csv(f"../results/ML_reports/all_scores.csv", index_col=[0,1])
+
+df = df[ df["Testing score"] > 0.70] # narrow down to where testing score is more than the value
+
+df = df[ df["Precision score"] > 0.72] # narrow down to where precision score is more than the value
+
+print(df.to_string())
+
